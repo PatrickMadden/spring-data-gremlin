64679fa98efc4f7d5ac292279ec03c2dad5512b7
PatrickMadden <pmadden@tomsawyer.com>
2018-09-25 16:37:37 -0500

    Provide GremlinQuery instance that doesn't require Criteria but instead can rely on a QueryScriptGenerator instance.

--- src/main/java/com/microsoft/spring/data/gremlin/query/query/GremlinQuery.java
+++ src/main/java/com/microsoft/spring/data/gremlin/query/query/GremlinQuery.java
@@ -5,26 +5,43 @@
  */
 package com.microsoft.spring.data.gremlin.query.query;
 
-import com.microsoft.spring.data.gremlin.query.criteria.Criteria;
-import lombok.*;
-import org.springframework.util.Assert;
 
+import com.microsoft.spring.data.gremlin.query.criteria.Criteria;
+import org.springframework.util.Assert;
 import java.util.List;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
 
 public class GremlinQuery {
 
     @Getter
+    @Setter
     private final Criteria criteria;
-
-    @Setter(AccessLevel.PRIVATE)
     private QueryScriptGenerator generator;
 
-    public GremlinQuery(@NonNull Criteria criteria) {
-        this.criteria = criteria;
+    public GremlinQuery(QueryScriptGenerator generator)
+    {
+        this(null, generator);
     }
 
-    public void setScriptGenerator(@NonNull QueryScriptGenerator generator) {
-        this.setGenerator(generator);
+    public GremlinQuery(Criteria criteria) {
+        this(criteria, null);
+    }
+
+    public GremlinQuery(Criteria criteria, QueryScriptGenerator generator) {
+        this.criteria = criteria;
+        this.generator = generator;
+    }
+
+    public void setScriptGenerator(QueryScriptGenerator generator) {
+        this.generator = generator;
+    }
+
+    public QueryScriptGenerator getScriptGenerator() {
+        return this.generator;
     }
 
     public <T> List<String> doSentenceGenerate(@NonNull Class<T> domainClass) {
